b093f8bf0477fda4319374d2c38c2bbc2faee28e
Pham Tien <19127576@student.hcmus.edu.vn>
2022-03-23 22:23:03 +0700

    Adding Navigation Bottom

--- app/src/main/java/com/example/tingting/MainActivity.kt
+++ app/src/main/java/com/example/tingting/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.tingting
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.tingting.databinding.ActivityMainBinding
 
 
@@ -12,26 +14,48 @@ import com.example.tingting.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
 //  hddjwedhd
-    var button: Button?= null
-    lateinit var  adapters: Adapters
-    lateinit var viewpager: ViewPager2
+//    var button: Button?= null
+//    lateinit var  adapters: Adapters
+//    lateinit var viewpager: ViewPager2
     private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var list = mutableListOf<Int>()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
-        list.add(R.drawable.image1)
-        list.add(R.drawable.image2)
-        list.add(R.drawable.image3)
+        val navController = findNavController(R.id.fragment)
 
-        adapters = Adapters(this)
-        adapters.setContentList(list)
-        viewpager = findViewById(R.id.viewPagerMain)
-        viewpager.adapter = adapters
+
+        
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.firstFragment,
+                R.id.secondFragment,
+                R.id.thirdFragment,
+                R.id.fourFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+
+
+//        var list = mutableListOf<Int>()
+
+
+//        list.add(R.drawable.image1)
+//        list.add(R.drawable.image2)
+//        list.add(R.drawable.image3)
+//
+//        adapters = Adapters(this)
+//        adapters.setContentList(list)
+//        viewpager = findViewById(R.id.viewPagerMain)
+//        viewpager.adapter = adapters
 
     }
 }
