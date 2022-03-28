8c6e9bb891475100bce1d5542cf7f6100fb4caf5
Pham Tien <19127576@student.hcmus.edu.vn>
2022-03-28 18:34:01 +0700

    Add Toolbar

---
+++ app/src/main/java/com/example/tingting/UserInfoFragment.kt
@@ -0,0 +1,31 @@
+package com.example.tingting
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class UserInfoFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = UserInfoFragment()
+    }
+
+    private lateinit var viewModel: UserInfoViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.user_info_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(UserInfoViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/tingting/UserInfoViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.tingting
+
+import androidx.lifecycle.ViewModel
+
+class UserInfoViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
